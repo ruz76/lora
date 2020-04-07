29eb323989fefef7dd8c7a09ccf58f76d7e3714b
Jan Ruzicka <jan.ruzicka.1@plus4u.net>
2020-04-07 21:01:16 +0200

    TODO

--- python/getPosition.py
+++ python/getPosition.py
@@ -100,6 +100,7 @@ def get_position(gtws, gtts):
 
 def save_to_db(sensorid, coords_fixed, coords_fixed_wgs, coords_computed, measured):
     """Saves all information from one sensor into database."""
+    # TODO Save also computed coordinates
 
     distance = math.hypot(coords_fixed[0] - coords_computed[0], coords_fixed[1] - coords_computed[1])
 
